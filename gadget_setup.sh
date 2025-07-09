a08c4f1182b22c14e62f729f987d492134de4ddf
PsycoStea <105074722+PsycoStea@users.noreply.github.com>
2025-07-09 13:01:28 +0200

    Update gadget_setup.sh

--- gadget_setup.sh
+++ gadget_setup.sh
@@ -1,47 +1,69 @@
 #!/bin/bash
 set -e
 
-G="/sys/kernel/config/usb_gadget/g1"
-if [ -d "$G" ]; then
-  if [ -f "$G/UDC" ]; then
-    echo "" | sudo tee $G/UDC
+# --- Configuration ---
+# This is the partition that will be exposed as a USB drive.
+PARTITION="/dev/mmcblk0p3"
+
+GADGET_DIR="/sys/kernel/config/usb_gadget/g1"
+
+# --- Full Teardown ---
+if [ -d "$GADGET_DIR" ]; then
+    echo "Tearing down existing gadget..."
+    if [ -f "$GADGET_DIR/UDC" ] && [ ! -z "$(cat $GADGET_DIR/UDC)" ]; then
+        echo "" > "$GADGET_DIR/UDC"
+    fi
+    rm -rf "$GADGET_DIR"
     sleep 1
-  fi
-  sudo rm -rf "$G"
 fi
 
+# --- Clean Setup ---
+echo "Setting up composite gadget (HID + Mass Storage)..."
+modprobe libcomposite
+mkdir -p "$GADGET_DIR"
+cd "$GADGET_DIR"
 
-sudo modprobe libcomposite
+# 1. Set Device Descriptors
+echo 0x1d6b > idVendor      # Linux Foundation
+echo 0x0104 > idProduct     # Multifunction Composite Gadget
+echo 0x0100 > bcdDevice
+echo 0x0200 > bcdUSB
 
-cd /sys/kernel/config/usb_gadget
-sudo mkdir g1
-cd g1
+# 2. Add String Descriptors
+mkdir -p strings/0x409
+echo "badusb-03" > strings/0x409/serialnumber
+echo "Pi Zero" > strings/0x409/manufacturer
+echo "BadUSB + Storage" > strings/0x409/product
 
-echo 0x1d6b | sudo tee idVendor      # Linux Foundation
-echo 0x0104 | sudo tee idProduct     # Multifunction Composite Gadget
-echo 0x0100 | sudo tee bcdDevice
-echo 0x0200 | sudo tee bcdUSB
+# 3. Create Configuration
+mkdir -p configs/c.1/strings/0x409
+echo "HID + Mass Storage" > configs/c.1/strings/0x409/configuration
+echo 250 > configs/c.1/MaxPower
 
-sudo mkdir -p strings/0x409
-echo "fedcba9876543210" | sudo tee strings/0x409/serialnumber
-echo "Pi Zero 2W" | sudo tee strings/0x409/manufacturer
-echo "BadUSB" | sudo tee strings/0x409/product
+# 4. Create HID Keyboard Function
+mkdir -p functions/hid.usb0
+echo 1 > functions/hid.usb0/protocol
+echo 1 > functions/hid.usb0/subclass
+echo 8 > functions/hid.usb0/report_length
+echo -ne '\x05\x01\x09\x06\xa1\x01\x05\x07\x19\xe0\x29\xe7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x01\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x01\x95\x06\x75\x08\x15\x00\x25\x65\x05\x07\x19\x00\x29\x65\x81\x00\xc0' > functions/hid.usb0/report_desc
 
-sudo mkdir -p configs/c.1/strings/0x409
-echo "Default config" | sudo tee configs/c.1/strings/0x409/configuration
-echo 120 | sudo tee configs/c.1/MaxPower
+# 5. Create Mass Storage Function
+mkdir -p functions/mass_storage.usb0
+echo 1 > functions/mass_storage.usb0/stall
+echo 0 > functions/mass_storage.usb0/lun.0/cdrom
+echo 0 > functions/mass_storage.usb0/lun.0/ro
+echo 0 > functions/mass_storage.usb0/lun.0/nofua
+echo "$PARTITION" > functions/mass_storage.usb0/lun.0/file
 
-sudo mkdir -p functions/hid.usb0
-echo 1 | sudo tee functions/hid.usb0/protocol
-echo 1 | sudo tee functions/hid.usb0/subclass
-echo 8 | sudo tee functions/hid.usb0/report_length
+# 6. Associate functions with the configuration
+ln -s functions/hid.usb0 configs/c.1/
+ln -s functions/mass_storage.usb0 configs/c.1/
 
-# Standard HID keyboard report descriptor
-echo -ne '\x05\x01\x09\x06\xa1\x01\x05\x07\x19\xe0\x29\xe7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x01\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x01\x95\x06\x75\x08\x15\x00\x25\x65\x05\x07\x19\x00\x29\x65\x81\x00\xc0' | sudo tee functions/hid.usb0/report_desc
+# 7. Bind to UDC to activate
+UDC_CONTROLLER=$(ls /sys/class/udc | head -n 1)
+echo "$UDC_CONTROLLER" > UDC
 
-sudo ln -s functions/hid.usb0 configs/c.1/
+# 8. Set permissions for user-space script
+chmod 666 /dev/hidg0
 
-echo "$(ls /sys/class/udc)" | sudo tee UDC
-
-# Let normal users access
-sudo chmod 666 /dev/hidg0
+echo "Composite gadget setup complete."
